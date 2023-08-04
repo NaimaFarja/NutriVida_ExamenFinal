6703f81716346a73209cba1c987bbe4e7f86f88c
Flores Fabricio A <Flores Fabricio A@DESKTOP-GAT3E8H>
2023-08-04 01:29:12 -0300

    Testimonio
    Se crearon archivos java y html de testimonio

---
+++ NutriVida_ExamenFinal/src/main/java/ar/edu/unju/fi/repository/TestimonioRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unju.fi.entity.Testimonio;
+//holis
+public interface TestimonioRepository extends CrudRepository<Testimonio, Long> {
+
+}
