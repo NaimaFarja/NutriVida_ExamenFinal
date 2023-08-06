0d90594a74ab598b7522a8965802446220ccdcc2
Farja Naima Blanca <Farja Naima Blanca@DESKTOP-RNP7A0T>
2023-08-06 00:47:39 -0300

    Edicion de imagenes.

--- NutriVida_ExamenFinal/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
+++ NutriVida_ExamenFinal/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
@@ -3,6 +3,7 @@ package ar.edu.unju.fi.controller;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +21,7 @@ import ar.edu.unju.fi.service.UsuarioService;
  * Controlador que maneja las operaciones relacionadas con los testimonios de usuarios.
  */
 
+@Controller
 public class TestimonioController {
 
 	@Autowired
