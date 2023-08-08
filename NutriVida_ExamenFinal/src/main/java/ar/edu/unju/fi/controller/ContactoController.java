bd8894139ff915e5d7f12eedebbd817a7d1a6bfe
Luis Guerrero <beto_lage16@hotmail.com>
2023-08-08 15:56:04 -0500

    documentacion ContactoController

--- NutriVida_ExamenFinal/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ NutriVida_ExamenFinal/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -5,13 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+/**
+ * Esta clase es un controlador que maneja las solicitudes relacionadas con la página de contacto.
+ * 
+ */
 
 @Controller
 @RequestMapping("/")
 public class ContactoController {
+	
+	/**
+	 * Maneja la solicitud GET para acceder a la página de contacto.
+	 * 
+	 * @param model El objeto Model utilizado para pasar datos a la vista.
+	 * @return El nombre de la vista de la página de contacto ("contacto").
+	 */
+	
 		@GetMapping("/contacto")
-		public String getIndexController(Model model) {
+		public String getContactoController(Model model) {
 			return "contacto";
 		}
 }
